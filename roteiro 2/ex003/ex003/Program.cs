193757ee7f36a3b9dfeba0cacf163903c5227e2d
Pedro Neto <andradepedro2106@gmail.com>
2026-08-18 22:02:23 -0300

    Add files via upload

---
+++ roteiro 2/ex003/ex003/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        XDocument documento = XDocument.Load("estoque.xml");
+
+        foreach (XElement item in documento.Root.Elements("item"))
+        {
+            XElement nome = item.Element("nome");
+
+            if (nome != null && nome.Value == "Mouse")
+            {
+                item.Element("quantidade").Value = "10";
+
+                Console.WriteLine("Quantidade atualizada");
+            }
+        }
+
+        documento.Save("estoque.xml");
+
+    }
+}
---
+++ roteiro 2/ex004/ex004/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Xml;
+
+class Program
+{
+    static async Task Main()
+    {
+        string url = "https://www.w3schools.com/xml/simple.xml";
+
+        using (HttpClient client = new HttpClient())
+        {
+
+            string response = await client.GetStringAsync(url);
+
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(response);
+
+            XmlNodeList alimentos = doc.SelectNodes("//food");
+
+            foreach (XmlNode food in alimentos)
+            {
+                string nome = food.SelectSingleNode("name").InnerText;
+                string preco = food.SelectSingleNode("price").InnerText;
+
+                Console.WriteLine("Nome: " + nome);
+                Console.WriteLine("Preço: " + preco);
+            }
+        }
+    }
+}
