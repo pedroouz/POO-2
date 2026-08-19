a65cf44c91588a37c46a0db551c300cf44802dd6
Pedro Neto <andradepedro2106@gmail.com>
2026-08-18 22:00:04 -0300

    Add files via upload

---
+++ roteiro 2/ex002/ex002/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+public class Program
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+    public static void Main() { 
+
+        List<Produto> produtos = new List<Produto>();
+        produtos.Add(new Produto { Nome = "Pão", Preco = 1.0 });
+        produtos.Add(new Produto { Nome = "Espeto de frango", Preco = 10.00 });
+        produtos.Add(new Produto { Nome = "Água", Preco = 2.00 });
+
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Produto>));
+
+        using (StreamWriter writer = new StreamWriter("produtos.xml"))
+        {
+            serializer.Serialize(writer, produtos);
+        }
+
+        string conteudo = File.ReadAllText("produtos.xml");
+        Console.WriteLine(conteudo);
+
+    }
+
+}
+
