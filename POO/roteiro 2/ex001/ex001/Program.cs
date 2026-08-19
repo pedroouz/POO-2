3ee903893db7dc341675cec29f5e35120f1a05a8
Pedro Neto <andradepedro2106@gmail.com>
2026-08-18 21:58:13 -0300

    Add files via upload

---
+++ POO/roteiro 2/ex001/ex001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+public class Program
+{
+    [XmlRoot("alunos")]
+    public class Alunos
+    {
+        [XmlElement("aluno")]
+       public List<Aluno> alunos { get; set; }
+    }
+    public class Aluno
+    {
+        public string nome {  get; set; }
+
+        public string curso { get; set; }
+
+    }
+    static void Main()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Alunos));
+        using (StreamReader reader = new StreamReader("alunos.xml"))
+        {
+            Alunos lista = (Alunos)serializer.Deserialize(reader);
+
+            foreach(var aluno in lista.alunos)
+            {
+                Console.WriteLine(aluno.nome);
+                Console.WriteLine(aluno.curso);
+            }
+        }
+    }
+}
