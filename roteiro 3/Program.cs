1f059a6f27de008798d037840b471574baa272d5
Pedro Neto <andradepedro2106@gmail.com>
2026-09-01 20:48:38 -0300

    Add files via upload

---
+++ roteiro 3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        Livro livro = new Livro("A Paciente Silenciosa", "R.J Michaels", 2024);
+        string json = JsonConvert.SerializeObject(livro, Formatting.Indented);
+        Console.WriteLine(json);
+    }
+}
+
+class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int Ano {  get; set; }
+
+    public Livro (string titulo, string autor, int ano)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        Ano = ano;
+    }
+
+}
