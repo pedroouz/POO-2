e0dc462db86cea008364b2ee6c097a8a2f120b15
Pedro Neto <andradepedro2106@gmail.com>
2026-09-01 20:58:20 -0300

    Add files via upload

---
+++ roteiro 3/ex002/ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        string json = File.ReadAllText("aluno.json");
+
+        Aluno aluno = JsonConvert.DeserializeObject<Aluno>(json);
+
+        Console.WriteLine("Nome: " + aluno.Nome);
+        Console.WriteLine("Idade: " + aluno.Idade);
+        Console.WriteLine("Curso: " + aluno.Curso);
+    }
+}
+
+public class Aluno
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Curso { get; set; }
+}
+
