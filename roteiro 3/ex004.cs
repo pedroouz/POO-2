e7e0ef70a8250ab0ac8102c3cce9eeb00d23f75c
Pedro Neto <andradepedro2106@gmail.com>
2026-09-01 21:08:15 -0300

    Add files via upload

---
+++ roteiro 3/ex004.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string json = @"{
+            'Servidor': 'localhost',
+            'Porta': 3306,
+            'Usuario': 'root'
+        }";
+
+        JObject configuracao = JObject.Parse(json);
+
+        Console.WriteLine("Porta original: " + configuracao["Porta"]);
+
+        configuracao["Porta"] = 5432;
+
+        Console.WriteLine("Porta nova: " + configuracao["Porta"]);
+
+        Console.WriteLine("\nJSON atualizado:");
+        Console.WriteLine(configuracao.ToString());
+    }
+}
